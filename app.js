440cc01ceeb64dc6cda0faec7143efc44d794da6
r0hitsharma <15rohitsharma@gmail.com>
2016-01-16 18:49:43 +0100

    Adding cookie sessions.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('cookie-session');
 
 var routes = require('./routes/index');
 
@@ -12,9 +13,16 @@ var app = express();
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
+// body parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+// cookie sessions
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({secret : process.env.SESSION_SECRET}));
+
+// static file hosting
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
