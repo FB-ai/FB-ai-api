59f81cab3259e257793cb14dfcadaa2d0a349e47
r0hitsharma <15rohitsharma@gmail.com>
2016-01-16 21:19:17 +0100

    Fbgraph used for facebook authentication and session.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.status(200);
-  res.json({ title: 'Express' });
+var fb = require('../fb');
+
+// checks it the user has a session and lets through or pushes back to login
+function isAuthenticated(req, res, next){
+  // no authentication for these paths
+  if(
+    ['/startlogin']
+    .some(function(route){
+      return req.url.indexOf(route) != -1;
+    })
+  )
+    return next();
+
+  if(req.session.fbsk_state)
+    return next();
+
+  next({
+    status: 401,
+    message: 'No session on the server.'
+  });
+};
+
+router.use(isAuthenticated);
+
+// send to fb to start login process
+router.get('/startlogin', fb.redirectLoginForm);
+
+// receive callback from facebook
+router.get('/completelogin', function(req, res, next){
+  next({
+    status: 200,
+    success: true
+  });
+});
+
+// destroy session on the server
+router.all('/logout', function(req, res, next){
+  req.session = null;
+
+  next({
+    status: 200,
+    success: true
+  });
 });
 
 module.exports = router;
