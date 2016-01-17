4c661595546f7f245d317f66737602117ed6bc74
Mesterjagel <sebastian.thomsen@gmail.com>
2016-01-17 11:28:16 +0100

    removed test.txt. Added link table to contain current live edit

--- schema.pg.sql
+++ schema.pg.sql
@@ -1,57 +1,62 @@
 -- -----------------------------------------------------
 -- Table public.user
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS "public".user;
 
-CREATE TABLE "public".user (
-  id INT PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  token VARCHAR(255) NOT NULL,
-  organization VARCHAR(255) NOT NULL,
-  created TIMESTAMP NOT NULL DEFAULT(NOW()),
-  modified TIMESTAMP
+DROP TABLE IF EXISTS "public".updates;
+DROP TABLE IF EXISTS "public".contents_posts;
+DROP TABLE IF EXISTS "public".contents;
+DROP TABLE IF EXISTS "public".posts;
+DROP TABLE IF EXISTS "public".users;
+
+CREATE TABLE "public".users (
+  id serial PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  token varchar(255),
+  state varchar(255),
+  fb_id varchar(255) NOT NULL,
+  organization varchar(255) NOT NULL,
+  created timestamp DEFAULT(NOW()),
+  modified timestamp DEFAULT(NOW())
 );
 
 -- -----------------------------------------------------
 -- Table public.post
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS "public".post;
-
-CREATE TABLE "public".post (
-  id INT PRIMARY KEY NOT NULL,
-  user_id INT REFERENCES "user"(id)
+CREATE TABLE "public".posts (
+  id serial PRIMARY KEY,
+  user_id int REFERENCES "users"(id)
 );
 
 -- -----------------------------------------------------
 -- Table public.content
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS "public".content;
+CREATE TABLE "public".contents (
+  id serial PRIMARY KEY,
+  title varchar(255) NOT NULL,
+  text_body varchar(255) NOT NULL,
+  url varchar(255) NOT NULL,
+  created timestamp DEFAULT(NOW()),
+  post_id int REFERENCES posts(id)
+);
 
-CREATE TABLE "public".content (
-  id INT PRIMARY KEY NOT NULL,
-  title VARCHAR(255) NOT NULL,
-  text_body VARCHAR(255) NOT NULL,
-  url VARCHAR(255) NOT NULL,
-  created TIMESTAMP NOT NULL DEFAULT(NOW()),
-  post_id INT REFERENCES post(id)
+-- -----------------------------------------------------
+-- Table public.contents_posts
+-- -----------------------------------------------------
+CREATE TABLE "public".contents_posts (
+	id serial PRIMARY KEY,
+	content_id int REFERENCES contents(id),
+	post_id int REFERENCES posts(id),
+	created timestamp DEFAULT(NOW())
 );
 
 -- -----------------------------------------------------
 -- Table public.updates
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS "public".update;
-
-CREATE TABLE "public".update (
-  id INT PRIMARY KEY NOT NULL,
-  reach INT,
-  likes INT,
-  views INT,
-  post_id INT REFERENCES post(id),
-  content_id INT REFERENCES content(id)
+CREATE TABLE "public".updates (
+  id serial PRIMARY KEY,
+  reach int,
+  likes int,
+  views int,
+  post_id int REFERENCES posts(id),
+  content_id int REFERENCES contents(id)
 );
-
--- -----------------------------------------------------
--- Add foreign key
--- -----------------------------------------------------
-ALTER TABLE "public".post
-ADD COLUMN current_content_id INT REFERENCES content(id);
