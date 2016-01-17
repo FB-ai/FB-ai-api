8630749de617078adb0d385979e2d4a6507eadbb
r0hitsharma <15rohitsharma@gmail.com>
2016-01-17 11:02:31 +0100

    /completelogin fetches user details from facebook and saves it as user if id does not already exist.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var fb = require('../fb');
+var models = require('../models');
+var User = models.User;
 
 // checks it the user has a session and lets through or pushes back to login
 function isAuthenticated(req, res, next){
@@ -14,6 +16,7 @@ function isAuthenticated(req, res, next){
   )
     return next();
 
+  // console.log(req.session);
   if(req.session.fbsk_state)
     return next();
 
@@ -23,13 +26,68 @@ function isAuthenticated(req, res, next){
   });
 };
 
+// use authentication on this router
 router.use(isAuthenticated);
 
 // send to fb to start login process
 router.get('/startlogin', fb.redirectLoginForm);
 
 // receive callback from facebook
-router.get('/completelogin', function(req, res, next){
+router.get('/completelogin',
+  // regenerate auth token, also populates req.facebook
+  fb.authenticate,
+  function(req, res, next){
+  // console.log('facebook:', req.facebook);
+
+  // load user details from facebook
+  req.facebook.me(function(err, me){
+    if(err)
+      return next(err);
+    // console.log(err, user);
+    
+    User
+    .where({fb_id: me.id})
+    .fetch()
+    .then(function(user){
+      // return user if found
+      if(user)
+        return user;
+
+      // otherwise crate new user with fb retured 
+      try{
+        return User
+        .forge({
+          fb_id: me.id,
+          name: me.name,
+          token: req.session.fbsk_access_token,
+          state: req.session.fbsk_state,
+          // @TODO: maybe a registration page to add more details to the user
+          organization: 'Something'
+        })
+        .save();
+      }
+      catch(err){
+        next(err);
+      };
+    })
+    .then(function(user){
+      // console.log(user);
+      req.session.user = user.serialize();
+
+      next({
+        status: 200,
+        success: true,
+        user: user.serialize()
+      });
+    })
+    .catch(next);  
+  });
+});
+
+// router.all('/getprofile', function(req, res, next){});
+
+// dummy function for testing
+router.all('/', function(req, res, next){
   next({
     status: 200,
     success: true
