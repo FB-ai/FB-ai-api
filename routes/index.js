674a88bb1efdf9068ddfa2ff80e82e32cf9e1a99
r0hitsharma <15rohitsharma@gmail.com>
2016-01-17 13:07:23 +0100

    Middleware moved to it's own file.

--- routes/index.js
+++ routes/index.js
@@ -4,85 +4,26 @@ var router = express.Router();
 var fb = require('../fb');
 var models = require('../models');
 var User = models.User;
-
-// checks it the user has a session and lets through or pushes back to login
-function isAuthenticated(req, res, next){
-  // no authentication for these paths
-  if(
-    ['/startlogin']
-    .some(function(route){
-      return req.url.indexOf(route) != -1;
-    })
-  )
-    return next();
-
-  // console.log(req.session);
-  if(req.session.fbsk_state)
-    return next();
-
-  next({
-    status: 401,
-    message: 'No session on the server.'
-  });
-};
+var sessionMiddleware = require('../sessionMiddleware');
 
 // use authentication on this router
-router.use(isAuthenticated);
+router.use(sessionMiddleware.isAuthenticated);
 
 // send to fb to start login process
 router.get('/startlogin', fb.redirectLoginForm);
 
 // receive callback from facebook
 router.get('/completelogin',
-  // regenerate auth token, also populates req.facebook
   fb.authenticate,
+  sessionMiddleware.loadOrSaveUser,
   function(req, res, next){
-  // console.log('facebook:', req.facebook);
-
-  // load user details from facebook
-  req.facebook.me(function(err, me){
-    if(err)
-      return next(err);
-    // console.log(err, user);
-    
-    User
-    .where({fb_id: me.id})
-    .fetch()
-    .then(function(user){
-      // return user if found
-      if(user)
-        return user;
-
-      // otherwise crate new user with fb retured 
-      try{
-        return User
-        .forge({
-          fb_id: me.id,
-          name: me.name,
-          token: req.session.fbsk_access_token,
-          state: req.session.fbsk_state,
-          // @TODO: maybe a registration page to add more details to the user
-          organization: 'Something'
-        })
-        .save();
-      }
-      catch(err){
-        next(err);
-      };
-    })
-    .then(function(user){
-      // console.log(user);
-      req.session.user = user.serialize();
-
-      next({
-        status: 200,
-        success: true,
-        user: user.serialize()
-      });
-    })
-    .catch(next);  
-  });
-});
+    next({
+      status: 200,
+      success: true,
+      user: req.session.user
+    });
+  }
+);
 
 // router.all('/getprofile', function(req, res, next){});
 
@@ -91,6 +32,14 @@ router.all('/', function(req, res, next){
   next({
     status: 200,
     success: true
+  });
+});
+
+router.all('/user', function(req, res, next){
+  next({
+    status: 200,
+    success: true,
+    user: req.session.user
   });
 });
 
---
+++ sessionMiddleware.js
@@ -0,0 +1,74 @@
+var fb = require('./fb');
+var models = require('./models');
+var User = models.User;
+
+// checks it the user has a session and lets through or pushes back to login
+function isAuthenticated(req, res, next){
+  // no authentication for these paths
+  if(
+    ['/startlogin']
+    .some(function(route){
+      return req.url.indexOf(route) != -1;
+    })
+  )
+    return next();
+
+  console.log(req.session);
+  if(req.session.fbsk_state)
+    return next();
+
+  next({
+    status: 401,
+    message: 'No session on the server.'
+  });
+};
+
+function loadOrSaveUser(req, res, next){
+  // console.log('facebook:', req.facebook);
+
+  // load user details from facebook
+  req.facebook.me(function(err, me){
+    // console.log(err, me);
+    if(err)
+      return next(err);
+    
+    User
+    .where({fb_id: me.id})
+    .fetch()
+    .then(function(user){
+      // console.log('found user:', user);
+      // return user if found
+      if(user)
+        return user;
+
+      // otherwise crate new user with fb retured 
+      try{
+        return User
+        .forge({
+          fb_id: me.id,
+          name: me.name,
+          token: req.session.fbsk_access_token,
+          state: req.session.fbsk_state,
+          // @TODO: maybe a registration page to add more details to the user
+          organization: 'Something'
+        })
+        .save();
+      }
+      catch(err){
+        console.log('error saving user:', err);
+        next(err);
+      };
+    })
+    .then(function(user){
+      // console.log('found/created user:', user);
+      req.session.user = user.serialize();
+      next();
+    })
+    .catch(next);  
+  });
+};
+
+module.exports = {
+  isAuthenticated: isAuthenticated,
+  loadOrSaveUser: loadOrSaveUser
+};
